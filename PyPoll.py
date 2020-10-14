c24b4299d01ec74f272a599bd40e22e710436306
Arkhawan Salih <arkhawan80@gmail.com>
2020-10-14 00:18:16 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,6 @@
 import os
 import csv
 
-
 csvpath = os.path.join("..", "Resources", ["election_data.csv"])
 
 with open(csvpath, "rw")as csvfile:
@@ -13,8 +12,7 @@ with open(csvpath, "rw")as csvfile:
     percent_votes = []
     total_votes = 0
     
-    
-    
+   
     for votes in num_votes:
         total_vote = sum("voter_id").value()
         print(total_vote)
